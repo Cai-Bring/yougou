7b647f7dd3c8990a394a98d77c86f9b485d469c6
Cai-Bring <743489214@qq.com>
2020-02-28 19:56:24 +0800

    主页导航栏部分完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,11 @@
 import request from "../../utils/request.js"
 Page({
   data:{
-    banners:[]
+    banners:[], //轮播图
+    navs:[] // 导航
   },
   onLoad(){
+    // 请求得到轮播图
     request({
       url:'/home/swiperdata'
     }).then(res=>{
@@ -11,6 +13,23 @@ Page({
       const {message}=res.data
       this.setData({
         banners:message
+      })
+    })
+
+    // 请求得到导航部分
+    request({
+      url:'/home/catitems'
+    }).then(res=>{
+      // console.log(res)
+      const {message}=res.data
+      const navdata = message.map((v,i)=>{
+        if(i===0){
+          v.url ="/pages/category/index"
+        }
+        return v
+      })
+      this.setData({
+        navs: navdata
       })
     })
   }
