a1f7b22822bf2243cd21c9fdbbeedc2a7463af0f
Cai-Bring <743489214@qq.com>
2020-02-28 19:17:06 +0800

    轮播图部分完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,17 @@
-
+import request from "../../utils/request.js"
 Page({
+  data:{
+    banners:[]
+  },
+  onLoad(){
+    request({
+      url:'/home/swiperdata'
+    }).then(res=>{
+      // console.log(res)
+      const {message}=res.data
+      this.setData({
+        banners:message
+      })
+    })
+  }
 })
