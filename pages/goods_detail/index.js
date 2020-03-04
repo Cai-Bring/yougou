6c906ca0b2feec082daf6db3c2c4331cddf13ae1
Cai-Bring <743489214@qq.com>
2020-03-04 14:52:42 +0800

    商品详情页基本布局完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:{}
+    list:{},
+    current:0
   },
 
   /**
@@ -23,6 +24,27 @@ Page({
       this.setData({
         list:res.data.message
       })
+    })
+  },
+  // 切换TAB栏
+  goodnav(e){
+    // console.log(e)
+    const index = e.target.dataset.index
+    this.setData({
+      current:index
+    })
+  },
+  // 点击图片
+  choosePhoto(e){
+    const index = e.target.dataset.index
+    let list = this.data.list.pics.map(v=>{
+      return v.pics_big_url
+    })
+    // console.log(list)
+    // 显示图片大图
+    wx.previewImage({
+      current: list[index], // 当前显示图片的http链接
+      urls: list // 需要预览的图片http链接列表
     })
   }
 })
