dc73dc1cabe61fd40dd8710406a0d79c89a1eb74
Cai-Bring <743489214@qq.com>
2020-03-06 20:29:22 +0800

    购物车获取本地数据完成

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -4,19 +4,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    user:{}
+    user:{},
+    list:[] //本地存储 
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 第一次加入购物车页面，加载本地存储数据
+    this.setData({
+      list: wx.getStorageSync('goods')||[]
+    })
   },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // 每次再次加入购物车页面，都需要重新加载一次本地存储数据
+    this.setData({
+      list: wx.getStorageSync('goods') || []
+    })
   },
   // 获取地址
   choose_address(){
